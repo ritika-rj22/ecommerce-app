bff54cbc23461ea0403abd36cee82728afb89688
shwetanshusrivastava <47234729+shwetanshusrivastava@users.noreply.github.com>
2020-05-26 21:06:20 +0530

    Update cartActions.js

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { ADD_PRODUCT_TO_CART, REMOVE_PRODUCT_FROM_CART } from "./types"
+import { ADD_PRODUCT_TO_CART, REMOVE_PRODUCT_FROM_CART, INCREMENT_CART_ITEM_QUANTITY, DECREMENT_CART_ITEM_QUANTITY} from "./types"
 import ProductList from "../data/productList.json";
 
 export const addProductToCart = product => {
@@ -13,4 +13,18 @@ export const removeProductFromCart = productId => dispatch => {
         type: REMOVE_PRODUCT_FROM_CART,
         payload: productId
     })
+};
+
+export const incrementCartQuantity = productId => dispatch => {
+    dispatch({
+        type: INCREMENT_CART_ITEM_QUANTITY,
+        payload: productId
+    })
+};
+
+export const decrementCartQuantity = productId => dispatch => {
+  dispatch({
+      type: DECREMENT_CART_ITEM_QUANTITY,
+      payload: productId
+  })
 };
